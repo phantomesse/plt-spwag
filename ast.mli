159ae9d2397771ee72796cba4a3ef003544fe2eb
Yunhe (John) Wang <yhjw88@gmail.com>
2013-12-05 22:14:15 -0500

    Changed ast.mli

--- ast.mli
+++ ast.mli
@@ -4,26 +4,29 @@ type func_type = Slide | Comp | Attr | Func
 
 type null = Null
 
+type identifier = (* Identifiers *)
+    Identifier of string
+
 type parent = (* For inheritance of components *)
     Parent of identifier 
     | Noparent of null
-
-type identifier = (* Identifiers *)
-    Identifier of string
 
 type expr = (* Expressions *)
     | Binop of expr * operator * expr (* a + b *)
     | Litint of int (* 42 *)
     | Litper of int (* 42% *)
     | Litstr of string (* “foo” *)
-    | Litbool of boolean (* true *)
+    | Litbool of bool (* true *)
     | Assign of identifier * expr (* foo - 42 *) (*we should examine this*)
     | Call of func_call
     | Noexpr of null (* for double newlines *)
-    
-(* We need to figure out how to incorporate percents and null into the ast*)
-
-type stmt = (* Statements ; WIP *)
+(* Calls and executes function. Follows a control flow detailed in the LRM/Revisions doc *)
+and func_call = { (* Handles calls of functions, components, *)
+    name : identifier; (* Name of the function *)
+    actuals : expr list; (* Evaluated actual parameters *)
+    mods : stmt list; (* Additional statements *)
+}   
+and stmt = (* Statements ; WIP *)
     | Block of stmt list (* { ... } *)
     | Expr of expr (* foo = bar + 3; *)
     | Return of expr (* return 42; *)
@@ -36,9 +39,8 @@ type stmt = (* Statements ; WIP *)
 # body
      end
 *)
-
 type func_definition = { (* Handles declarations of functions, components, attributes, slides *)
-    type: func_type; (* e.g. slide, component, attribute, func *)
+    t: func_type; (* e.g. slide, component, attribute, func *)
     name : identifier; (* Name of the function *)
     formals : identifier list; (* Name of the formal parameters *)
     inheritance : parent; (* Name of any parent components, ie box, or null *)
@@ -46,12 +48,4 @@ type func_definition = { (* Handles declarations of functions, components, attri
     body : stmt list; (* Conditional, Return Statements, Function Declarations/Calls, etc. *)
 }
 
-
-(* Calls and executes function. Follows a control flow detailed in the LRM/Revisions doc *)
-type func_call = { (* Handles calls of functions, components, *)
-    name : identifier; (* Name of the function *)
-    actuals : expr list; (* Evaluated actual parameters *)
-    mods : stmt list; (* Additional statements *)
-}
-
-type program = identifier list * func_decl list (* global vars, funcs*)
+type program = identifier list * func_definition list (* global vars, funcs*)
