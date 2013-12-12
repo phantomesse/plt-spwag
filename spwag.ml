47bef7a147d4e8da80fe8a7c013efc5116fd74b5
Aftab Khan <aftabjkhan@gmail.com>
2013-12-12 15:58:56 -0500

    modified spwag.ml to serve as top-level compilation task distributor and main entry-point to spwag compiler

--- spwag.ml
+++ spwag.ml
@@ -1,10 +1,18 @@
-open Ir
+type action = Ast | Irgenerator | Compile
 
 let _ =
-    let input =
-        if Array.length Sys.argv > 1 then
-            open_in Sys.argv.(1)
-    else stdin in
-        let lexbuf = Lexing.from_channel input in
-        let ir = Ir.program Scanner.token lexbuf in
-            print_string "Hello!"
+  let action = if Array.length Sys.argv > 1 then
+    List.assoc Sys.argv.(1) [ ("-a", Ast);
+			      ("-i", Irgenerator);
+			      ("-c", Compile) ]
+  else Compile in
+  let lexbuf = Lexing.from_channel stdin in
+  let program = Parser.program Scanner.token lexbuf in
+  match action with
+    Ast -> let listing = Ast.string_of_program program
+    (* TODO: Implement Ast.string_of_program function in ast.ml *)
+           in print_string listing
+  | Irgenerator -> ignore (Irgenerator.run program)
+  | Compile -> Compile.translate program
+    (* TODO: Implement Compile.translate function in Compile.ml *)
+ 
