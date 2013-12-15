5a93ae9f888d755173d3eb055f82c1e0055df7c9
Lauren Zou <zou.lauren@gmail.com>
2013-12-15 05:24:11 -0500

    Added one-fish-two-fish test case and added next and prev functionality in javascript.

--- config/config.js
+++ config/config.js
@@ -1,4 +1,26 @@
+$(document).ready(function() {
+    slideId = getCurrentSlide();
+    displaySlide(slideId);
+
+    // Detect hash change
+    $(window).bind('hashchange', function(e) {
+        slideId = getCurrentSlide();
+        displaySlide(slideId);
+    });
+});
+
+function getCurrentSlide() {
+    var hash = window.location.hash;
+    return hash === ''? 'main' : hash.substring(1);
+}
+
+function displaySlide(slideId) {
+    console.log(slideId);
+    $('.slide:visible:not(#' + slideId + ')').hide();
+    $('#' + slideId).show();
+}
+
 $('a.prev').click(function() {
     var prev_slide = $(this).attr('href');
     console.log(prev_slide);
-})
+});
