3674dfe1f92762be1e2ec9cddf9e32adab8aadce
Yunhe (John) Wang <yhjw88@gmail.com>
2013-12-15 19:50:57 -0500

    Removed some bars from ast

--- ast.ml
+++ ast.ml
@@ -25,7 +25,7 @@ type func_call = {
 
 (* Expressions *)
 and expr =
-     Binop of expr * operator * expr (* a + b *)
+    Binop of expr * operator * expr (* a + b *)
 	| Notop of expr (* !a only applies to booleans *)
     | Litint of int (* 42 *)
     | Litper of int (* 42% *)
@@ -38,7 +38,7 @@ and expr =
 
 (* Calls and executes function. Follows a control flow detailed in the LRM/Revisions doc *)
 and stmt = (* Statements ; WIP *)
-     Block of stmt list (* { ... } *)
+    Block of stmt list (* { ... } *)
     | Expr of expr (* foo = bar + 3; *)
     | Return of expr (* return 42; *)
     | If of expr * stmt * stmt (* if (foo == 42) stmt1 else stmt2 end *)
@@ -90,7 +90,7 @@ and string_of_expr = function
         String.concat "" (List.map (fun ex -> "[" ^ (string_of_expr ex) ^ "]") e)
     | Call(f) -> string_of_call f
 and string_of_stmt = function
-    | Block(stmts) -> "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+    Block(stmts) -> "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
     | Expr(expr) -> string_of_expr expr ^ "\n";
     | Return(expr) -> "return " ^ string_of_expr expr ^ "\n";
     | If(e, s, Block([])) -> "if " ^ string_of_expr e ^ "\n" ^ string_of_stmt s
@@ -104,7 +104,7 @@ let string_of_vdecl = function
     Identifier(s) -> "var " ^ s ^ "\n"
 
 let string_of_function_type = function
-    | Slide -> "define slide"
+    Slide -> "define slide"
     | Comp -> "define comp"
     | Attr -> "define attr"
     | Func -> "define func"
