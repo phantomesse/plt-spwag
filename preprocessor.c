8a7e20161cfead7ff8949936b0e8e2ceff931588
Aftab Khan <aftabjkhan@gmail.com>
2013-12-19 21:47:13 -0500

    cleaned up code, added comments for clarity

--- preprocessor.c
+++ preprocessor.c
@@ -1,4 +1,10 @@
-/* Aftab Khan */
+/* Author: Aftab Khan */
+
+/* 
+ * Reads SPWAG source files (without curly braces "{}") from Standard In
+ * and inserts curly braces as appropriate. Preprocessed SPWAG code is
+ * printed to Standard Out.
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,8 +13,7 @@
 #define BUF_SIZE 1024
 
 void printNTabs(int n){
-	for (int i = 0; i < n; ++i)
-	{
+	for (int i = 0; i < n; ++i){
 		printf(" ");
 	}
 }
@@ -16,10 +21,14 @@ void printNTabs(int n){
 
 int main (int argc, char** argv){
 	char buffer[BUF_SIZE];
-	char lineBuffer[BUF_SIZE + 2];
-	char* tabCharSet = "\t "; // Space, Tab characters
+	//char lineBuffer[BUF_SIZE + 2];
+	
+	// Valid Tab Characters: Tab (\t), Space
+	char* tabCharSet = "\t ";
 	size_t contentSize = 1; // includes NULL
 
+	//TO BE USED IF OUTPUTTING TO A FILE IS DESIRED:
+	/*
 	char *content = malloc(sizeof(char) * BUF_SIZE);
 	if(content == NULL){
 		perror("Failed to allocate content");
@@ -27,7 +36,8 @@ int main (int argc, char** argv){
 	}
 
 	content[0] = '\0'; // make null-terminated
-	
+	*/
+
 	int oldTabLength = -1;
 	int tabLength = 0;
 
@@ -38,16 +48,13 @@ int main (int argc, char** argv){
 		tabLength = strspn(buffer, tabCharSet);
 		//printf("\ttabLength = %i, oldTabLength = %i\n", tabLength, oldTabLength);
 		if(tabLength > oldTabLength){
-			//strcat(buffer, "{\n");
 			printNTabs(oldTabLength);
 			printf("{\n%s", buffer);
 		} else if (tabLength < oldTabLength){
-			//strcat(buffer, "}\n");
 			printNTabs(tabLength);
 			printf("}\n%s", buffer);
 		} else printf("%s", buffer);
 		//printf("%s\n", buffer);
-		
 
 		//TO BE USED IF OUTPUTTING TO A FILE IS DESIRED:
 		/*char *old = content;
@@ -59,8 +66,9 @@ int main (int argc, char** argv){
 			exit(2);
 		}
 		strcat(content, buffer);*/
-
 	}
+
+	// Prints the final curly brace, if the file contained any lines
 	if (oldTabLength > -1) printf("\n}\n");
 
 	if(ferror(stdin)){
