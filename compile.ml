18c0ac0cd751e18f4875644168062d7cda5b54ba
Lauren Zou <zou.lauren@gmail.com>
2013-12-20 03:58:44 -0500

    Cleaned up methods, added font decoration function, display function, and fixed tabbing in HTML.

--- compile.ml
+++ compile.ml
@@ -7,135 +7,159 @@ open Ir.Element
 open Ir.Slide
 module StringMap = Map.Make(String)
 
-let string_of_property property value =
+(* Inserts the appropriate number of tabs *)
+let rec tab number =
+    if number == 0 then ""
+    else "    " ^ tab (number - 1)
+
+(* Translates a CSS property given the property and its value *)
+let string_of_css_property property value =
     if String.length value > 0 then
-        "            " ^ property ^ ": " ^ value ^ ";\n"
-    else
-        ""
+        tab 3 ^ property ^ ": " ^ value ^ ";\n"
+    else ""
 ;;
 
-let string_of_css style classname =
-    "        #" ^ classname ^ " {\n" ^
+(* Translates a CSS display property *)
+let string_of_css_display_property display = match display with
+    | true -> ""
+    | false -> tab 3 ^ "display: hidden;\n"
 
-    (*string_of_property "display", string_of_bool elem.display ^*)
+(* Translates a CSS font decoration property *)
+let string_of_css_font_decoration decoration = match decoration with
+    | "" -> ""
+    | "italic" -> tab 3 ^ "font-style: italic;\n"
+    | "bold" -> tab 3 ^ "font-weight: bold;\n"
+    | "underline" -> tab 3 ^ "text-decoration: underline;\n"
+    | _ -> ""
+
+(* Translates the CSS of an element given the CSS style and id *)
+let string_of_element_css (style:Element.css) id =
+    tab 2 ^ "#" ^ id ^ " {\n" ^
+
+    string_of_css_display_property style.display ^
     
-    string_of_property "left" style.position_x ^
-    string_of_property "top" style.position_y ^
+    string_of_css_property "left" style.position_x ^
+    string_of_css_property "top" style.position_y ^
 
-    string_of_property "margin-top" style.margin_top ^
-    string_of_property "margin-bottom" style.margin_bottom ^
-    string_of_property "margin-left" style.margin_left ^
-    string_of_property "margin-right" style.margin_right ^
+    string_of_css_property "margin-top" style.margin_top ^
+    string_of_css_property "margin-bottom" style.margin_bottom ^
+    string_of_css_property "margin-left" style.margin_left ^
+    string_of_css_property "margin-right" style.margin_right ^
 
-    string_of_property "padding-top" style.padding_top ^
-    string_of_property "padding-bottom" style.padding_bottom ^
-    string_of_property "padding-left" style.padding_left ^
-    string_of_property "padding-right" style.padding_right ^
+    string_of_css_property "padding-top" style.padding_top ^
+    string_of_css_property "padding-bottom" style.padding_bottom ^
+    string_of_css_property "padding-left" style.padding_left ^
+    string_of_css_property "padding-right" style.padding_right ^
 
-    string_of_property "color" style.text_color ^
-    string_of_property "background-color" style.background_color ^
+    string_of_css_property "color" style.text_color ^
+    string_of_css_property "background-color" style.background_color ^
 
-    string_of_property "font-family" style.font ^
-    string_of_property "font-size" style.font_size ^
-    (* Need to handle font decoration with italics, bold, and underline *)
+    string_of_css_property "font-family" style.font ^
+    string_of_css_property "font-size" style.font_size ^
+    string_of_css_font_decoration style.font_decoration ^
 
-    string_of_property "border-width" style.border ^
-    string_of_property "border-color" style.border_color ^
+    string_of_css_property "border-width" style.border ^
+    string_of_css_property "border-color" style.border_color ^
 
-    string_of_property "width" style.width ^
-    string_of_property "height" style.height ^
+    string_of_css_property "width" style.width ^
+    string_of_css_property "height" style.height ^
 
-    "        }"
+    tab 2 ^ "}\n"
 ;;
 
-let string_of_slide_css style classname =
-    "        #" ^ classname ^ " {\n" ^
-
-    (*string_of_property "display", string_of_bool elem.display ^*)
+(* Translates the CSS of a slide given the CSS style *)
+let string_of_slide_css (style:Slide.slide_css) classname =
+    tab 2 ^ "#" ^ classname ^ " {\n" ^
     
-    string_of_property "padding-top" style.padding_top ^
-    string_of_property "padding-bottom" style.padding_bottom ^
-    string_of_property "padding-left" style.padding_left ^
-    string_of_property "padding-right" style.padding_right ^
+    string_of_css_property "padding-top" style.padding_top ^
+    string_of_css_property "padding-bottom" style.padding_bottom ^
+    string_of_css_property "padding-left" style.padding_left ^
+    string_of_css_property "padding-right" style.padding_right ^
 
-    string_of_property "color" style.text_color ^
-    string_of_property "background-color" style.background_color ^
+    string_of_css_property "color" style.text_color ^
+    string_of_css_property "background-color" style.background_color ^
 
-    string_of_property "font-family" style.font ^
-    string_of_property "font-size" style.font_size ^
-    (* Need to handle font decoration with italics, bold, and underline *)
+    string_of_css_property "font-family" style.font ^
+    string_of_css_property "font-size" style.font_size ^
+    string_of_css_font_decoration style.font_decoration ^
 
-    string_of_property "border-width" style.border ^
-    string_of_property "border-color" style.border_color ^
+    string_of_css_property "border-width" style.border ^
+    string_of_css_property "border-color" style.border_color ^
 
-    "        }"
+    tab 2 ^ "}\n"
 ;;
 
+(* Retrieves the styles from an element and its children elements *)
 let rec get_css_from_element (element, element_id) =
-    string_of_css element.Element.style element_id ^
+    "\n" ^ string_of_element_css element.Element.style element_id ^
 
     (* Get the CSS from the elements of this element *)
-    String.concat "" (List.map get_css_from_element (StringMap.fold (fun id element l -> (element, element_id ^ "-" ^ id)::l) element.Element.elements []))
+    String.concat "\n" (List.map get_css_from_element (StringMap.fold (fun id element l -> (element, element_id ^ "-" ^ id)::l) element.Element.elements []))
 ;;
 
+(* Retrieves the styles from a slide and its children elements *)
 let get_css_from_slide slide =
     (* Get the CSS from the slide *)
-    string_of_slide_css slide.Slide.style slide.Slide.id ^ "\n\n" ^
+    "\n" ^ string_of_slide_css slide.Slide.style slide.Slide.id ^ 
 
     (* Get the CSS from the elements of this slide *)
-    String.concat "\n\n" (List.map get_css_from_element (StringMap.fold (fun id element l -> (element, slide.Slide.id ^ "-" ^ id)::l) slide.Slide.elements []))
+    String.concat "\n" (List.map get_css_from_element (StringMap.fold (fun id element l -> (element, slide.Slide.id ^ "-" ^ id)::l) slide.Slide.elements []))
 ;;
 
-let string_of_text text = 
+(* Translates a string of text to text *)
+let string_of_text text tab_level = 
     if String.length text > 0 then
-        "            " ^ text ^ "\n"
-    else
-        ""
+        tab (tab_level + 1) ^ text ^ "\n"
+    else ""
 ;;
 
-let string_of_image image = 
+(* Translates an image url to an <img> tag *)
+let string_of_image image tab_level = 
     if String.length image > 0 then
-        "            <img src=\"" ^ image ^ " />\n"
-    else
-        ""
+        tab (tab_level + 1) ^ "<img src=\"" ^ image ^ " />\n"
+    else ""
 ;;
 
+(* Translates next and prev slides into <a> tags *)
 let string_of_next_prev next_prev slide =
     if String.length slide > 0 then
-        "        <a class=\"" ^ next_prev ^ "\" href=\"#" ^ slide ^ "\"></a>\n"
-    else
-        ""
+        tab 2 ^ "<a class=\"" ^ next_prev ^ "\" href=\"#" ^ slide ^ "\"></a>\n"
+    else ""
 ;;
 
-let rec get_html_from_element (element, element_id) =
-    "        <div id=\"" ^ element_id ^ "\" class=\"box\">\n" ^
+(* Retrieves the HTML from an element and its children elements *)
+let rec get_html_from_element (element, element_id, tab_level) =
+    tab tab_level ^ "<div id=\"" ^ element_id ^ "\" class=\"box\">\n" ^
 
-    string_of_text element.text ^
-    string_of_image element.image ^
+    string_of_text element.text tab_level ^
+    string_of_image element.image tab_level ^
 
     (* Get the HTML from the elements of this element *)
-    String.concat "\n" (List.map get_html_from_element (StringMap.fold (fun id element l -> (element, element_id ^ "-" ^ id)::l) element.Element.elements [])) ^ "\n" ^
+    String.concat "\n\n" (List.map get_html_from_element (StringMap.fold (fun id element l -> (element, element_id ^ "-" ^ id, tab_level + 1)::l) element.Element.elements [])) ^
 
-    "        </div>"
+    tab tab_level ^ "</div>\n"
 ;;
 
+(* Retrieves the HTML from a slide and its children elements *)
 let get_html_from_slide slide =
-    "    <div id=\"" ^ slide.Slide.id ^ "\" class=\"slide\">\n" ^
+    tab 1 ^ "<div id=\"" ^ slide.Slide.id ^ "\" class=\"slide\">\n" ^
 
     (* Get the HTML from each element of this slide *)
-    String.concat "\n\n" (List.map get_html_from_element (StringMap.fold (fun id element l -> (element, slide.Slide.id ^ "-" ^id)::l) slide.Slide.elements [])) ^ "\n\n" ^
+    String.concat "\n\n" (List.map get_html_from_element (StringMap.fold (fun id element l -> (element, slide.Slide.id ^ "-" ^id, 2)::l) slide.Slide.elements [])) ^
 
     (* Next and prev *)
     string_of_next_prev "prev" slide.Slide.prev ^
     string_of_next_prev "next" slide.Slide.next ^
 
-    "    </div>"
+    tab 1 ^ "</div>"
 ;;
 
+(* Translates an identifier into a string *)
 let string_of_identifier = function
     Identifier(s) -> s
 ;;
 
+(* Translates an operator into a string *)
 let string_of_operator operator = match operator with
     | Plus -> "+"
     | Minus -> "-"
@@ -148,6 +172,7 @@ let string_of_operator operator = match operator with
     | Or -> "||"
     | And -> "&&"
 
+(* Translates an expression into a string *)
 let rec string_of_expression (expression_detail, expression_type) = match expression_detail with
     | Binop (expr1, operator, expr2) -> string_of_expression expr1 ^ " " ^ string_of_operator operator ^ " " ^ string_of_expression expr2
     | Notop expr -> "!(" ^ string_of_expression expr ^ ")"
@@ -162,28 +187,29 @@ let rec string_of_expression (expression_detail, expression_type) = match expres
     | Call func_call -> string_of_identifier func_call.cname ^ "(" ^ String.concat ", " (List.map (fun expr -> string_of_expression expr) func_call.actuals) ^ ");"
 ;;
 
-let rec translate_statements_to_javascript statement = match statement with
-    | Block stmt -> String.concat "\n            " (List.map translate_statements_to_javascript stmt)
+(* Translates a statement into a string *)
+let rec string_of_statement statement = match statement with
+    | Block stmt -> String.concat "\n            " (List.map string_of_statement stmt)
     | Expr expr -> string_of_expression expr
     | Return expr -> "return " ^ string_of_expression expr ^ ";"
-    | If (expr, stmt1, stmt2) -> "if (" ^ string_of_expression expr ^ ") {\n" ^ translate_statements_to_javascript stmt1 ^ "\n} else {\n" ^ translate_statements_to_javascript stmt2 ^ "\n}\n" (* if (foo == 42) stmt1 else stmt2 end *)
-    | While (expr, stmt) -> "while (" ^ string_of_expression expr ^ ") {\n" ^ translate_statements_to_javascript stmt ^ "\n}\n"
+    | If (expr, stmt1, stmt2) -> "if (" ^ string_of_expression expr ^ ") {\n" ^ string_of_statement stmt1 ^ "\n} else {\n" ^ string_of_statement stmt2 ^ "\n}\n" (* if (foo == 42) stmt1 else stmt2 end *)
+    | While (expr, stmt) -> "while (" ^ string_of_expression expr ^ ") {\n" ^ string_of_statement stmt ^ "\n}\n"
     | Declaration identifier -> "var " ^ string_of_identifier identifier ^ ";"
     | Decassign (identifier, expr) -> "var " ^ string_of_identifier identifier ^ " = " ^ string_of_expression expr ^ ";"
 ;;
 
-let translate_to_javascript script =
+(* Translates the script into JavaScript *)
+let get_javascript script =
     (* Function definition *)
-    "        function " ^ string_of_identifier script.name ^ "(" ^
+    "function " ^ string_of_identifier script.name ^ "(" ^
 
     (* Formals *)
     String.concat ", " (List.map (fun identifier -> string_of_identifier identifier) script.formals) ^ ") {\n"^
 
     (* Statements *)
-    "            " ^
-    String.concat "\n            " (List.map translate_statements_to_javascript script.body) ^ "\n" ^
+    String.concat "\n" (List.map string_of_statement script.body) ^ "\n" ^
 
-    "        }"
+    "}"
 ;;
 
 let compile (slides, identifiers, scripts) =
@@ -192,14 +218,14 @@ let compile (slides, identifiers, scripts) =
     "<head>\n" ^
 
     (* If we have time, we should abstract out the config paths *)
-    "    <link rel=\"stylesheet\" type=\"text/less\" href=\"../../config/config.css\">\n" ^
+    tab 1 ^ "<link rel=\"stylesheet\" type=\"text/less\" href=\"../../config/config.css\">\n" ^
 
-    "    <style type=\"text/css\">\n" ^
+    tab 1 ^ "<style type=\"text/css\">\n" ^
 
     (* Abstract out all of the CSS *)
     String.concat "\n" (List.map get_css_from_slide slides) ^ "\n" ^
 
-    "    </style>\n" ^
+    tab 1 ^ "</style>\n" ^
 
     "</head>\n\n" ^
     "<body>\n" ^
@@ -207,15 +233,15 @@ let compile (slides, identifiers, scripts) =
     (* HTML components such as slides and elements *)
     String.concat "\n" (List.map get_html_from_slide slides) ^ "\n\n" ^
 
-    "    <script type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"></script>\n" ^
-    "    <script type=\"text/javascript\" src=\"../../config/config.js\"></script>\n\n" ^
+    tab 1 ^ "<script type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"></script>\n" ^
+    tab 1 ^ "<script type=\"text/javascript\" src=\"../../config/config.js\"></script>\n\n" ^
 
-    "    <script>\n" ^
+    tab 1 ^ "<script>\n" ^
 
     (* Javascript goes here *)
-    String.concat "\n" (List.map translate_to_javascript scripts) ^ "\n" ^
+    String.concat "\n" (List.map get_javascript scripts) ^ "\n" ^
     
-    "    </script>\n\n" ^
+    tab 1 ^ "</script>\n\n" ^
 
     "</body>\n\n" ^
     "</html>"
